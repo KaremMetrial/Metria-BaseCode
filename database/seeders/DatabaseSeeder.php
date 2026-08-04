b8965fb532dd74b1aa6ee2512e3eaf8af83e965c
Karem Metrial <karim.metrial@gmail.com>
2026-08-04 11:29:40 +0300

    feat: register EnterpriseDemoSeeder in DatabaseSeeder
    Add the EnterpriseDemoSeeder class to the list of seeders called
    during database seeding so that enterprise demo data is included
    when running the seeders.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolesAndPermissionsSeeder::class,
             TerritorySeeder::class,
+            EnterpriseDemoSeeder::class,
         ]);
     }
 }
